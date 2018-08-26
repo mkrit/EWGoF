d6263ef68b110281af46bf00b4324beec3c040f0
MeryamK <meryam.krit@gmail.com>
2018-08-26 18:53:57 +0200

    Start
    Additions of old files

---
+++ src/GoFsim.cpp
@@ -0,0 +1,14 @@
+#include<Rcpp.h>
+using namespace Rcpp;
+// [[Rcpp::export]]
+NumericVector GoFsim(int nsim, int n, Function fun) {
+  NumericVector res(nsim);
+  NumericVector V(n);
+  for(int i=0;i<nsim;i++){
+    V =-log(runif(n));
+    res[i]=as<double>(fun(V));
+  }
+ // NumericVector temp=wrap(res);
+  std::sort(res.begin(),res.end());
+  return res;
+  }
